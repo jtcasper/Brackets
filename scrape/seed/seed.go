bf1584c6f26f0493add201b0a5ed643244de4625
Jacob Casper <jacob.thomas.casper@gmail.com>
2020-04-13 23:46:57 -0500

    Make seed dogfood the api

--- scrape/seed/seed.go
+++ scrape/seed/seed.go
@@ -2,17 +2,13 @@ package main
 
 import (
 	"git.jacobcasper.com/brackets/client"
-	"git.jacobcasper.com/brackets/db"
 	_ "github.com/mattn/go-sqlite3"
 	"log"
+	"net/http"
+	"net/url"
 )
 
 func main() {
-
-	db, err := db.New()
-	if err != nil {
-		log.Fatal("Could not open db: ", err.Error())
-	}
 
 	client, err := client.Get()
 	if err != nil {
@@ -25,12 +21,11 @@ func main() {
 		tracks, err := client.GetPlaylistTracks(playlist.ID)
 		if err != nil {
 			log.Printf("Couldn't retrieve playlist %s.", string(playlist.ID))
+			continue
 		}
 		for _, trackPage := range tracks.Tracks {
 			for _, artist := range trackPage.Track.Artists {
-				db.Mu.Lock()
-				db.Db.Exec("INSERT INTO ARTIST (ID, NAME) VALUES (?, ?)", artist.ID, artist.Name)
-				db.Mu.Unlock()
+				http.PostForm("http://localhost:8080/artist/add", url.Values{"artist_id": {string(artist.ID)}})
 			}
 		}
 	}
